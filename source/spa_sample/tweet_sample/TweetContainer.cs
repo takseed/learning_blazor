90d6faa626623e865ec36a297d63203c3b544d97
takseed <takseed64@outlook.jp>
2020-11-27 00:02:53 +0900

    ToExploreMode 調整

---
+++ source/spa_sample/tweet_sample/TweetContainer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace tweet_sample
+{
+    public class TweetData
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+    }
+    public class TweetContainer
+    {
+        private int Counter = 0;
+        public List<TweetData> Tweets { get; set; } = new List<TweetData>();
+        public void AddTweet(string s)
+        {
+            Tweets.Add(new TweetData { Id = ++Counter, Content = s });
+        }
+    }
+}
